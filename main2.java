2608897f7b467fbeb18da5541b25ddb2532b8c4a
NicolasTorquato <nicotorqua@gmail.com>
2022-04-19 22:05:48 -0300

    comativb

---
+++ main2.java
@@ -0,0 +1,13 @@
+public class main2 {
+    public static void main(String[] args){
+        int k = 1000;
+        int soma = 0;
+        while(k>0){
+            if(k%3 == 0 || k%5 == 0){
+                soma = soma+k;
+            }
+            k = k-1;
+            }
+            System.out.println("O valor é:" + soma);
+    }
+}
